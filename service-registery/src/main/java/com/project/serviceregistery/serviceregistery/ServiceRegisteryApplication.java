9448884b8db22d19ef63e6c327b590bbff517008
priyanka1404 <priyankareddynomula111@gmail.com>
2024-04-19 16:36:46 -0500

    SERVICE REGISTRY CHANGES

--- service-registery/src/main/java/com/project/serviceregistery/serviceregistery/ServiceRegisteryApplication.java
+++ service-registery/src/main/java/com/project/serviceregistery/serviceregistery/ServiceRegisteryApplication.java
@@ -1,11 +1,11 @@
-package com.example.serviceregistery;
+package com.project.serviceregistery.serviceregistery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer // to make our spring boot application a eureka server-based service Registery
+@EnableEurekaServer
 public class ServiceRegisteryApplication {
 
 	public static void main(String[] args) {
