ea9174ed39b36cf87794483a54693c8188f2b4f9
priyanka1404 <priyankareddynomula111@gmail.com>
2024-04-23 19:48:37 -0500

    Spring cloud  bus  module rabbitmq Msg broker changes

---
+++ employee-service/src/main/java/com/project/employeeservice/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.project.employeeservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope // will force this spring bean to reload the configuration
+// it will trigger the refresh the event
+@RestController
+public class MessageController {
+    @Value("${spring.boot.message}")
+    private String message;
+
+    @GetMapping("/users/message")
+    public String message(){
+
+        return message;
+    }
+}
